b72ab2378ce3dbb0e59b0829f154bbe57ad17699
hqxdj <934282093@qq.com>
2019-07-25 10:46:01 +0800

    测试

---
+++ src/main/java/com/cloud/common/util/TestGit.java
@@ -0,0 +1,13 @@
+package com.cloud.common.util;
+
+/**
+ * @author xdj
+ * @version V1.0
+ * @ProjectName: cloud-common
+ * @Description: 试试
+ * @Date 2019/7/25 10:44
+ * Copyright (c)   xdj
+ */
+
+public class TestGit {
+}
